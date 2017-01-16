b04b76a7d5932c46bb3c166149be5721cc2f06b1
Kaleb Haslam <kalebmarc@hotmail.com>
2017-01-16 18:27:49 +0000

    add cleanup and default task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
 
 gulp.task('sass', function(){
 	return gulp.src('app/style.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('dist/'))
+		.pipe(gulp.dest('dist/'));
 });
 
 gulp.task('build', function(){
 	gulp.src(['app/**/*'])
 		.pipe(gulp.dest('dist/'));
 });
-
+gulp.task('cleanup', function(){
+	del(['dist/*.scss']);
+});
 gulp.task('default', function(){
-
-	
+	gulp.start('sass');
+	gulp.start('build');
+	gulp.start('cleanup');
 });
